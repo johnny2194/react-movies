386e909d1f199cf29480209757e5e26eac4f449b
Mungo Dewar <mpjdewar@gmail.com>
2017-07-11 15:27:29 +0100

    Add on change listner

--- client/src/components/movieSelector.jsx
+++ client/src/components/movieSelector.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 
 class MovieSelector extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {search: ""}
+
+  }
+
+  handleChange(event) {
+    this.setState({search: event.target.value})
+  }
+
   render() {
     return (
       <form>
-        <input type="text" placeholder="Search for actor" ></input>
+        <input type="text" placeholder="Search for actor" onChange={this.handleChange.bind(this)}></input>
       </form>
     )
   }
