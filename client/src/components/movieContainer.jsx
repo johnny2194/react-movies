b9b21c91efe8a055a3205fc2c68951d05308caa6
Mungo Dewar <mpjdewar@gmail.com>
2017-07-11 14:48:32 +0100

    Render select from movieSelector

--- client/src/components/movieContainer.jsx
+++ client/src/components/movieContainer.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import MovieSelector from './movieSelector'
 
 class MovieContainer extends React.Component {
 
   render() {
-    return <h1>Hello world! I ike you</h1>
+    return (
+    <div>
+      <h1>Sup bra</h1>
+      <MovieSelector />
+    </div>
+    )
   }
+
 
 }
 
---
+++ client/src/components/movieSelector.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class MovieSelector extends React.Component {
+
+  render() {
+    return <select></select>
+  }
+}
+
+export default MovieSelector 
